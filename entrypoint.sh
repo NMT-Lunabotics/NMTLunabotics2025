28b31fb716e09c4b6d68072315517c3294fea48d
nuc <nuc@example.com>
2024-11-05 18:06:21 -0700

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ source /home/luna/ros2_ws/install/setup.bash
 export ROS_DOMAIN_ID=1
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
+sudo chmod 666 /dev/ttyACM0
+
 # Launch the ROS nodes, including the RealSense D455 camera
 exec "$@"
 
