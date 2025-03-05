06d19300172ad85a6110fe57559f1d2b0fc34c3d
Niall Devlin <niallcdevlin@gmail.com>
2025-03-04 20:16:48 -0700

    rtabmap

--- ros2_ws/src/slam_config/launch/rtabmap_launch.py
+++ ros2_ws/src/slam_config/launch/rtabmap_launch.py
@@ -6,69 +6,78 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
-ARGUMENTS = [
-    DeclareLaunchArgument('rviz', default_value='false',
-                          choices=['true', 'false'], description='Start RViz'),
-    DeclareLaunchArgument('rtabmapviz', default_value='false', choices=['true', 'false'],
-                          description='Start RTAB-Map visualizer'),
-    DeclareLaunchArgument('localization', default_value='false', choices=['true', 'false'],
-                          description='Start RTAB-Map in localization mode (meaning that a map is already made)'),
-    DeclareLaunchArgument('nav2', default_value='true', choices=['true', 'false'],
-                          description='Start Navigation2')
-]
-
 def generate_launch_description():
     # Path to the RealSense camera and RTAB-Map launch files
     realsense_launch_file = os.path.join(
         get_package_share_directory(
             'realsense2_camera'), 'launch', 'rs_launch.py'
     )
+
+    rplidar_launch_file = os.path.join(
+        get_package_share_directory('rplidar_ros'),
+        'launch',
+        'rplidar_s1_launch.py'
+    )
+
     rtabmap_launch_file = os.path.join(
         get_package_share_directory(
             'rtabmap_launch'), 'launch', 'rtabmap.launch.py'
     )
 
-    # Launch arguments
-    return LaunchDescription([
-        # RealSense D435 camera launch
-        IncludeLaunchDescription(
+    realsense_launch = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(realsense_launch_file),
             launch_arguments={
                 'depth_module.depth_profile': '640x480x30',
                 'rgb_camera.color_profile': '640x480x30',
                 'enable_depth': 'true',
-                # 'enable_infra1': 'true',
-                # 'enable_infra2': 'true',
                 'enable_color': 'true',
+                'enable_rgbd': 'true',
                 'enable_gyro': 'true',
                 'enable_accel': 'true',
                 'enable_sync': 'true',
+                'align_depth.enable': 'true',
                 'unite_imu_method': '2'
             }.items()
-        ),
+        )
+    
+    rplidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rplidar_launch_file),
+    )
 
-        # RTAB-Map SLAM launch with visual odometry
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(rtabmap_launch_file),
-            launch_arguments={
-                'depth_topic': '/camera/camera/depth/image_rect_raw',
-                'rgb_topic': '/camera/camera/color/image_raw',
-                'camera_info_topic': '/camera/camera/color/camera_info',
-                'imu_topic': '/camera/camera/imu',
-                # 'left_image_topic': '/camera/camera/infra1/image_rect_raw',
-                # 'left_camera_info_topic': '/camera/camera/infra1/camera_info',
-                # 'right_image_topic': '/camera/camera/infra2/image_rect_raw',
-                # 'right_camera_info_topic': '/camera/camera/infra2/camera_info'
-                'frame_id': 'camera_link',
-                'subscribe_depth': 'true',
-                'subscribe_rgb': 'true',
-                'subscribe_stereo': 'false',
-                'visual_odometry': 'true',
-                'approx_sync': 'false',
-                'queue_size': '10',
-                'rtabmap_viz': 'false',
-                'database_path': ''
-            }.items()
-        ),
+    imu_filter_node = Node(
+        package='imu_filter_madgwick', executable='imu_filter_madgwick_node', output='screen',
+        parameters=[{'use_mag': False, 
+                        'world_frame':'enu', 
+                        'publish_tf':False}],
+        remappings=[('imu/data_raw', '/camera/camera/imu')])
 
+    rtabmap_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rtabmap_launch_file),
+        launch_arguments={
+            'depth_topic': '/camera/camera/aligned_depth_to_color/image_raw',
+            'rgb_topic': '/camera/camera/color/image_raw',
+            'camera_info_topic': '/camera/camera/color/camera_info',
+            'imu_topic': '/imu/data',
+            'scan_topic': '/scan',
+            'wait_imu_to_init': 'true',
+            'frame_id': 'camera_link',
+            'subscribe_depth': 'true',
+            'subscribe_rgbd': 'true',
+            'subscribe_stereo': 'false',
+            'subscribe_scan': 'true',
+            'visual_odometry': 'true',
+            'approx_sync': 'false',
+            'queue_size': '10',
+            'rtabmap_viz': 'false',
+            'database_path': ''
+        }.items()
+    )
+
+
+    # Launch arguments
+    return LaunchDescription([
+        rplidar_launch,
+        realsense_launch,
+        imu_filter_node,
+        rtabmap_launch
     ])
