4da57cfa0fbc86a4e9ac30930ae4e3c4b8457579
Niall Devlin <niallcdevlin@gmail.com>
2025-03-13 20:25:22 -0600

    system control

--- arduino/I2C_test/I2C_test.ino
+++ arduino/I2C_test/I2C_test.ino
@@ -1,18 +1,26 @@
 #include <Wire.h>
 
-#define ADDRESS             0x58                       // Address of MD04 driver
+#define ADDRESS             0x59                       // Address of MD04 driver
 #define CMDBYTE             0x00                       // Command byte
 #define SPEEDBYTE           0x02                       // Byte to write to speed register
+
 void setup(){
-  Serial.begin(19200);                             
+  Serial.begin(9600);                             
   delay(100);
   
   Wire.begin();                                        // Start I2C connection
+
+  sendData(SPEEDBYTE, 255);
+  sendData(CMDBYTE, 2);
+  delay(8000);
+
+  sendData(SPEEDBYTE, 255);
+  sendData(CMDBYTE, 1);
+  delay(8000);
 }
 
 void loop(){
- sendData(SPEEDBYTE, 255);                             // Send speed data
-    sendData(CMDBYTE, 1);                              // Send Run Direction Command     
+ 
 }
 
 void sendData(byte commandRegister, byte value){       // Send data through I2C communication
