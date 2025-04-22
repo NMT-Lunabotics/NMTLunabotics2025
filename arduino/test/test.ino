ad619ecae06875d5e8bdb9515b93c4cfdbc23604
Niall Devlin <niallcdevlin@gmail.com>
2025-04-22 17:59:08 -0600

    motor stuff

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,16 +1,22 @@
+
+#include "helpers.hpp"
+#include "arduino_lib.hpp"
+
+OutPin test(9);
+
 void setup() {
     // Initialize serial communication at 9600 baud
     Serial.begin(9600);
-    pinMode(9, OUTPUT);
+
 }
 
 void loop() {
-    for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
-        analogWrite(9, dutyCycle);
-        delay(10); // Wait for 10ms
+    for (int i = 0; i < 255; i++) {
+        test.write_pwm_raw(i);
+        delay(10);
     }
-    for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-        analogWrite(9, dutyCycle);
-        delay(10); // Wait for 10ms
+    for (int i = 255; i >= 0; i--) {
+        test.write_pwm_raw(i);
+        delay(10);
     }
 }
