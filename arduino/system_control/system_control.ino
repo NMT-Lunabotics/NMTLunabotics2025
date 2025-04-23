bfdcd0327f67731453effc5a1e047dfa7135ad3d
Niall Devlin <niallcdevlin@gmail.com>
2025-04-22 19:20:17 -0600

    uncomments

--- arduino/system_control/system_control.ino
+++ arduino/system_control/system_control.ino
@@ -3,53 +3,52 @@
 #include <Servo.h>
 
 // Debug mode flag
-// bool debug_mode = false;
-// bool calibrate_actuators_flag = false;
-//TODO check pins
+bool debug_mode = false;
+bool calibrate_actuators_flag = false;
 
 //////// ACTUATORS ////////
 //Left side is L, right side is R, both is LR, bucket is B
 // I2C addresses for actuators
-// #define AL_I2C_ADDRESS 0x5A // B0
-// #define AR_I2C_ADDRESS 0x59 // B4
-// #define AB_I2C_ADDRESS 0x58 // B2
+#define AL_I2C_ADDRESS 0x5A // B0
+#define AR_I2C_ADDRESS 0x59 // B4
+#define AB_I2C_ADDRESS 0x58 // B2
 
 // I2C registers for actuators
-// #define SPEED_REG 0x02
-// #define DIR_REG 0x00
+#define SPEED_REG 0x02
+#define DIR_REG 0x00
 
 // Actuator potentiometer read pins
-// #define POTL_PIN A1
-// #define POTR_PIN A0
-// #define POTB_PIN A2
+#define POTL_PIN A1
+#define POTR_PIN A0
+#define POTB_PIN A2
 
 // Actuator info
 // Actuator stroke in mm
-// #define ALR_STROKE 191
-// #define AB_STROKE 140
+#define ALR_STROKE 191
+#define AB_STROKE 140
 
 // Actuator Calibration
-// #define AL_POT_MIN 53
-// #define AL_POT_MAX 883
-// #define AR_POT_MIN 3
-// #define AR_POT_MAX 837
-// #define AB_POT_MIN 30
-// #define AB_POT_MAX 782
+#define AL_POT_MIN 53
+#define AL_POT_MAX 883
+#define AR_POT_MIN 3
+#define AR_POT_MAX 837
+#define AB_POT_MIN 30
+#define AB_POT_MAX 782
 
-// float act_max_vel = 25; //mm/s
-// float act_max_error = 3; // mm
+float act_max_vel = 25; //mm/s
+float act_max_error = 3; // mm
 
 // Actuator targets
-// int aL_speed = 0;
-// int aR_speed = 0;
-// int aB_speed = 0;
+int aL_speed = 0;
+int aR_speed = 0;
+int aB_speed = 0;
 
-// float aL_pos = 0;
-// float aR_pos = 0;
-// float aB_pos = 0;
+float aL_pos = 0;
+float aR_pos = 0;
+float aB_pos = 0;
 
-// int aLR_tgt = -1;
-// int aB_tgt = -1;
+int aLR_tgt = -1;
+int aB_tgt = -1;
 
 //////// MOTORS ////////
 const int DACL1_PIN = 6;
@@ -64,22 +63,22 @@ int mL_speed = 0;
 int mR_speed = 0;
 
 //TODO implement servo logic
-// #define SERVO_PIN 12
+#define SERVO_PIN 12
 
 // Servo
-// bool servo_state = false;
+bool servo_state = false;
 
 // LED's
 // TODO implement
-// #define LEDR_PIN 2
-// #define LEDY_PIN 4
-// #define LEDG_PIN 7
-// #define LEDB_PIN 8
+#define LEDR_PIN 2
+#define LEDY_PIN 4
+#define LEDG_PIN 7
+#define LEDB_PIN 8
 
-// bool led_r = false;
-// bool led_y = false;
-// bool led_g = false;
-// bool led_b = false;
+bool led_r = false;
+bool led_y = false;
+bool led_g = false;
+bool led_b = false;
 
 // Timing
 int update_rate = 10; //hz
@@ -92,21 +91,21 @@ unsigned long current_time = 0;
 const unsigned long estop_timeout = 1000; // 1 second timeout
 unsigned long last_message_time = 0;
 bool emergency_stop = false;
-// bool doomsday = false;
+bool doomsday = false;
 
 // Set up PID controllers
-// PID pidL(2.2, 0.0022, 0.34, 2.0);
-// PID pidR(1.85, 0.0018, 0.31, 1.7);
-// PID pidB(3.0, 0.001, 0.4);
-// float vel_gain = 2.0;
+PID pidL(2.2, 0.0022, 0.34, 2.0);
+PID pidR(1.85, 0.0018, 0.31, 1.7);
+PID pidB(3.0, 0.001, 0.4);
+float vel_gain = 2.0;
 
 // Set up actuators
-// Actuator act_left(AL_I2C_ADDRESS, SPEED_REG, DIR_REG, POTL_PIN, false, 
-//                     ALR_STROKE, AL_POT_MIN, AL_POT_MAX, act_max_vel, pidL);
-// Actuator act_right(AR_I2C_ADDRESS, SPEED_REG, DIR_REG, POTR_PIN, false, 
-//                     ALR_STROKE, AR_POT_MIN, AR_POT_MAX, act_max_vel, pidR);
-// Actuator act_bucket(AB_I2C_ADDRESS, SPEED_REG, DIR_REG, POTB_PIN, false, 
-//                     AB_STROKE, AB_POT_MIN, AB_POT_MAX, act_max_vel, pidB);
+Actuator act_left(AL_I2C_ADDRESS, SPEED_REG, DIR_REG, POTL_PIN, false, 
+                    ALR_STROKE, AL_POT_MIN, AL_POT_MAX, act_max_vel, pidL);
+Actuator act_right(AR_I2C_ADDRESS, SPEED_REG, DIR_REG, POTR_PIN, false, 
+                    ALR_STROKE, AR_POT_MIN, AR_POT_MAX, act_max_vel, pidR);
+Actuator act_bucket(AB_I2C_ADDRESS, SPEED_REG, DIR_REG, POTB_PIN, false, 
+                    AB_STROKE, AB_POT_MIN, AB_POT_MAX, act_max_vel, pidB);
 
 // Set up motors
 OutPin motor_left_dac1(DACL1_PIN);
@@ -117,14 +116,14 @@ Motor motor_left(motor_left_dac1, motor_left_dac2, motor_max_vel, false);
 Motor motor_right(motor_right_dac1, motor_right_dac2, motor_max_vel, true);
 
 // Set up LEDs
-// OutPin ledr_pin(LEDR_PIN);
-// OutPin ledy_pin(LEDY_PIN);
-// OutPin ledg_pin(LEDG_PIN);
-// OutPin ledb_pin(LEDB_PIN);
+OutPin ledr_pin(LEDR_PIN);
+OutPin ledy_pin(LEDY_PIN);
+OutPin ledg_pin(LEDG_PIN);
+OutPin ledb_pin(LEDB_PIN);
 
 // Set up servo
-// OutPin servo_pin(SERVO_PIN);
-// Servo servo;
+OutPin servo_pin(SERVO_PIN);
+Servo servo;
 
 void processMessage(byte* data, int length);
 
@@ -132,10 +131,7 @@ void setup(){
     Serial.begin(115200);
     Serial.flush();
     // Wire.begin();
-    //  if (calibrate_actuators_flag) {
-        //  calibrateActuators(act_left, act_right, act_bucket);
-    //  }
-    // servo.attach(SERVO_PIN);
+    servo.attach(SERVO_PIN);
     // servo.write(0);
 }
 
@@ -227,21 +223,16 @@ void loop() {
         last_feedback_time = current_time;
 
         // Send feedback
-        // if (doomsday) {
-        //     Serial.println("Doomsday");
-        // }
+        if (doomsday) {
+            Serial.println("Doomsday");
+        }
         if (emergency_stop) {
             Serial.println("Estopped");
         }
-        Serial.println("<F," + String(mL_speed) + "," + String(mR_speed) + ">");
-        // Serial.println("<F," + String(aL_pos) + "," + String(aR_pos) + "," + String(aB_pos)
-        //     + "," + String(aL_speed) + "," + String(aR_speed) + ',' + String(aLR_tgt)
-        //     + "," + String(mL_speed) + "," + String(mR_speed) + ">");
-        // Serial.print("aL_pos:");
-        // Serial.print(aL_pos);
-        // Serial.print(",");
-        // Serial.print("aR_pos:");
-        // Serial.println(aR_pos);
+        // Serial.println("<F," + String(mL_speed) + "," + String(mR_speed) + ">");
+        Serial.println("<F," + String(aL_pos) + "," + String(aR_pos) + "," + String(aB_pos)
+            + "," + String(aL_speed) + "," + String(aR_speed) + ',' + String(aLR_tgt)
+            + "," + String(mL_speed) + "," + String(mR_speed) + ">");
     }
 
     // if (current_time - last_reset_int_time >= 1000 / reset_int_rate) {
@@ -255,72 +246,72 @@ void loop() {
 
 void processMessage(byte* data, int length) {
     char type = data[0];
-    // if (debug_mode) {
-    //     Serial.print("Received message of type: ");
-    //     Serial.println(type);
-    //     Serial.print("Length: ");
-    //     Serial.println(length);
-    //     Serial.print("Data: ");
-    //     for (int i = 0; i < length; i++) {
-    //         Serial.print(data[i], HEX);
-    //         Serial.print(" ");
-    //     }
-    //     Serial.println();
-    // }
+    if (debug_mode) {
+        Serial.print("Received message of type: ");
+        Serial.println(type);
+        Serial.print("Length: ");
+        Serial.println(length);
+        Serial.print("Data: ");
+        for (int i = 0; i < length; i++) {
+            Serial.print(data[i], HEX);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
 
     switch (type) {
         case 'A': { // Actuator control
-            // aLR_tgt = (int16_t)((data[1] << 8) | data[2]);  // Adjusted index to skip the type byte
-            // aB_tgt = (int16_t)((data[3] << 8) | data[4]);
-            // aL_speed = (int8_t)data[5];
-            // aR_speed = aL_speed;
-            // aB_speed = (int8_t)data[6];
-            // if (debug_mode) {
-            //     Serial.print("Arm Position: ");
-            //     Serial.println(aLR_tgt);
-            //     Serial.print("Bucket Position: ");
-            //     Serial.println(aB_tgt);
-            //     Serial.print("Arm Velocity: ");
-            //     Serial.println(aL_speed);
-            //     Serial.print("Bucket Velocity: ");
-            //     Serial.println(aB_speed);
-            // }
+            aLR_tgt = (int16_t)((data[1] << 8) | data[2]);  // Adjusted index to skip the type byte
+            aB_tgt = (int16_t)((data[3] << 8) | data[4]);
+            aL_speed = (int8_t)data[5];
+            aR_speed = aL_speed;
+            aB_speed = (int8_t)data[6];
+            if (debug_mode) {
+                Serial.print("Arm Position: ");
+                Serial.println(aLR_tgt);
+                Serial.print("Bucket Position: ");
+                Serial.println(aB_tgt);
+                Serial.print("Arm Velocity: ");
+                Serial.println(aL_speed);
+                Serial.print("Bucket Velocity: ");
+                Serial.println(aB_speed);
+            }
             break;
         }
         case 'M': { // Motor control
             mL_speed = (int8_t)data[1];  // Adjusted index to skip the type byte
             mR_speed = (int8_t)data[2];
-            // if (debug_mode) {
-            //     Serial.print("Left Speed: ");
-            //     Serial.println(mL_speed);
-            //     Serial.print("Right Speed: ");
-            //     Serial.println(mR_speed);
-            // }
+            if (debug_mode) {
+                Serial.print("Left Speed: ");
+                Serial.println(mL_speed);
+                Serial.print("Right Speed: ");
+                Serial.println(mR_speed);
+            }
             break;
         }
         case 'S': { // Servo control
-            // servo_state = data[1];  // Adjusted index to skip the type byte
-            // if (debug_mode) {
-            //     Serial.print("Servo State: ");
-            //     Serial.println(servo_state);
-            // }
+            servo_state = data[1];  // Adjusted index to skip the type byte
+            if (debug_mode) {
+                Serial.print("Servo State: ");
+                Serial.println(servo_state);
+            }
             break;
         }
         case 'L': { // LED control
-            // led_r = data[1];  // Adjusted index to skip the type byte
-            // led_y = data[2];
-            // led_g = data[3];
-            // led_b = data[4];
-            // if (debug_mode) {
-            //     Serial.print("Red: ");
-            //     Serial.println(led_r);
-            //     Serial.print("Yellow: ");
-            //     Serial.println(led_y);
-            //     Serial.print("Green: ");
-            //     Serial.println(led_g);
-            //     Serial.print("Blue: ");
-            //     Serial.println(led_b);
-            // }
+            led_r = data[1];  // Adjusted index to skip the type byte
+            led_y = data[2];
+            led_g = data[3];
+            led_b = data[4];
+            if (debug_mode) {
+                Serial.print("Red: ");
+                Serial.println(led_r);
+                Serial.print("Yellow: ");
+                Serial.println(led_y);
+                Serial.print("Green: ");
+                Serial.println(led_g);
+                Serial.print("Blue: ");
+                Serial.println(led_b);
+            }
             break;
         }
         default:
@@ -328,25 +319,3 @@ void processMessage(byte* data, int length) {
             break;
     }
 }
-
-// void calibrateActuators(Actuator act_left, Actuator act_right, Actuator act_bucket) {
-//     act_left.set_speed(act_max_vel);
-//     act_right.set_speed(act_max_vel);
-//     act_bucket.set_speed(act_max_vel);
-//     delay(10000);
-//     act_left.calibrate_pot();
-//     act_right.calibrate_pot();
-//     act_bucket.calibrate_pot();
-//     act_left.set_speed(-act_max_vel);
-//     act_right.set_speed(-act_max_vel);
-//     act_bucket.set_speed(-act_max_vel);
-//     delay(10000);
-//     Serial.println("Calibration complete: Biases set");
-//     Serial.print("Left Actuator: ");
-//     Serial.println(act_left.pos_mm());
-//     Serial.print("Right Actuator: ");
-//     Serial.println(act_right.pos_mm());
-//     Serial.print("Bucket Actuator: ");
-//     Serial.println(act_bucket.pos_mm());
-//     Serial.println("Calibration Complete");
-// }
