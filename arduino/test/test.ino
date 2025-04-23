b3e26067aebd426d628bc9145e0632cdbd98812b
Niall Devlin <niallcdevlin@gmail.com>
2025-04-22 18:39:49 -0600

    motor stuff

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,21 +1,24 @@
-
 #include "helpers.hpp"
 #include "arduino_lib.hpp"
 
 int max_vel = 30;
-Motor test(OutPin(6), OutPin(9), max_vel, false);
+Motor leftMotor(OutPin(6), OutPin(9), max_vel, false);
+Motor rightMotor(OutPin(3), OutPin(5), max_vel, true);
+
 void setup() {
     // Initialize serial communication at 9600 baud
     Serial.begin(9600);
-
 }
+
 void loop() {
     for (int i = -max_vel; i <= max_vel; i++) {
-        test.motor_ctrl(i);
+        leftMotor.motor_ctrl(i);
+        rightMotor.motor_ctrl(i);
         delay(100);
     }
     for (int i = max_vel; i >= -max_vel; i--) {
-        test.motor_ctrl(i);
+        leftMotor.motor_ctrl(i);
+        rightMotor.motor_ctrl(i);
         delay(100);
     }
 }
