75f551dc4d66f0c284d7858e655a07b658f16199
Niall Devlin <niallcdevlin@gmail.com>
2025-04-29 17:56:48 -0600

    actuator iprovements

--- arduino/system_control/system_control.ino
+++ arduino/system_control/system_control.ino
@@ -108,7 +108,7 @@ Actuator act_left(AL_I2C_ADDRESS, SPEED_REG, DIR_REG, POTL_PIN, false,
 Actuator act_right(AR_I2C_ADDRESS, SPEED_REG, DIR_REG, POTR_PIN, false, 
                     ALR_STROKE, AR_POT_MIN, AR_POT_MAX, act_max_vel, pidR);
 Actuator act_bucket(AB_I2C_ADDRESS, SPEED_REG, DIR_REG, POTB_PIN, false, 
-                    AB_STROKE, AB_POT_MIN, AB_POT_MAX, act_max_vel, pidB, 30, 100); // 11, 115
+                    AB_STROKE, AB_POT_MIN, AB_POT_MAX, act_max_vel, pidB, 30, 115); // 11, 115
 
 // Set up motors
 OutPin motor_left_dac1(DACL1_PIN);
