fa8dc4bca5ca91a5b63bb4d6e4b4075048526100
Niall Devlin <niallcdevlin@gmail.com>
2025-05-15 17:05:21 -0600

    fix

--- ros2_ws/src/slam_config/launch/realsense_launch.py
+++ ros2_ws/src/slam_config/launch/realsense_launch.py
@@ -4,28 +4,28 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-        realsense_launch_file = os.path.join(
-            get_package_share_directory(
-                'realsense2_camera'), 'launch', 'rs_launch.py'
-        )
-        realsense_launch = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(realsense_launch_file),
-            launch_arguments={
-                'depth_module.depth_profile': '640x480x30',
-                'rgb_camera.color_profile': '640x480x30',
-                'enable_depth': 'true',
-                'enable_pointcloud': 'true',
-                'enable_color': 'true',
-                'enable_rgbd': 'false',
-                'enable_gyro': 'true',
-                'enable_accel': 'true',
-                'enable_sync': 'true',
-                'align_depth.enable': 'true',
-                'unite_imu_method': '2',
-                'serial_no': '_306322300659',
-                'camera_name': 'camera_1',
-            }.items()
-        )
+    realsense_launch_file = os.path.join(
+        get_package_share_directory(
+            'realsense2_camera'), 'launch', 'rs_launch.py'
+    )
+    realsense_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(realsense_launch_file),
+        launch_arguments={
+            'depth_module.depth_profile': '640x480x30',
+            'rgb_camera.color_profile': '640x480x30',
+            'enable_depth': 'true',
+            'enable_pointcloud': 'true',
+            'enable_color': 'true',
+            'enable_rgbd': 'false',
+            'enable_gyro': 'true',
+            'enable_accel': 'true',
+            'enable_sync': 'true',
+            'align_depth.enable': 'true',
+            'unite_imu_method': '2',
+            'serial_no': '_306322300659',
+            'camera_name': 'camera_1',
+        }.items()
+    )
     return LaunchDescription([
         realsense_launch
     ])
