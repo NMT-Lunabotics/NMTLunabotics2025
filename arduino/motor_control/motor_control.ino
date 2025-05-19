cdf16cf0362e423a2586828d7cbb6c4cfe5c1489
Niall Devlin <niallcdevlin@gmail.com>
2025-05-18 18:05:00 -0600

    goliath

--- arduino/motor_control/motor_control.ino
+++ arduino/motor_control/motor_control.ino
@@ -13,7 +13,12 @@ const float rpm_to_pwm_constant = 5;
 int mL_speed = 0;
 int mR_speed = 0;
 
+int update_rate = 200; // Hz
+unsigned long last_update_time = 0;
+unsigned long current_time = 0;
+
 void processMessage(byte* data, int length);
+void set_speed(int pwm_pin, int dir_pin1, int dir_pin2, int speed);
 
 void setup() {
   Serial.begin(115200);  // Set baud rate to match Python script
@@ -41,10 +46,34 @@ void loop() {
       if (Serial.read() == 0x03) { // End byte
         processMessage(data, length);
       } else {
+        set_speed(motor1_pwm_pin, motor1_dir_pin1, motor1_dir_pin2, 0);
+        set_speed(motor2_pwm_pin, motor2_dir_pin1, motor2_dir_pin2, 0);
         Serial.println("End byte not found");
       }
     }
   }
+
+  current_time = millis();
+  if (current_time - last_update_time >= 1000 / update_rate) {
+    last_update_time = current_time;
+
+    // Set motor speeds
+    set_speed(motor1_pwm_pin, motor1_dir_pin1, motor1_dir_pin2, mL_speed);
+    set_speed(motor2_pwm_pin, motor2_dir_pin1, motor2_dir_pin2, mR_speed);
+  }
+
+}
+
+void set_speed(int pwm_pin, int dir_pin1, int dir_pin2, int speed) {
+  int pwm_value = constrain(abs(speed) * rpm_to_pwm_constant, 0, 255);
+  if (speed >= 0) {
+    digitalWrite(dir_pin1, HIGH);
+    digitalWrite(dir_pin2, LOW);
+  } else {
+    digitalWrite(dir_pin1, LOW);
+    digitalWrite(dir_pin2, HIGH);
+  }
+  analogWrite(pwm_pin, pwm_value);
 }
 
 void processMessage(byte* data, int length) {
@@ -53,29 +82,6 @@ void processMessage(byte* data, int length) {
     case 'M': { // Motor control
       mL_speed = (int8_t)data[1];
       mR_speed = (int8_t)data[2];
-
-      // Motor 1 (left)
-      int pwm_motor1 = constrain(abs(mL_speed) * rpm_to_pwm_constant, 0, 255);
-      if (mL_speed >= 0) {
-        digitalWrite(motor1_dir_pin1, HIGH);
-        digitalWrite(motor1_dir_pin2, LOW);
-      } else {
-        digitalWrite(motor1_dir_pin1, LOW);
-        digitalWrite(motor1_dir_pin2, HIGH);
-      }
-      analogWrite(motor1_pwm_pin, pwm_motor1);
-
-      // Motor 2 (right)
-      int pwm_motor2 = constrain(abs(mR_speed) * rpm_to_pwm_constant, 0, 255);
-      if (mR_speed >= 0) {
-        digitalWrite(motor2_dir_pin1, HIGH);
-        digitalWrite(motor2_dir_pin2, LOW);
-      } else {
-        digitalWrite(motor2_dir_pin1, LOW);
-        digitalWrite(motor2_dir_pin2, HIGH);
-      }
-      analogWrite(motor2_pwm_pin, pwm_motor2);
-
       break;
     }
     default:
